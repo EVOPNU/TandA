1069735c3025a0c865dbdd5dfefbe11aee6e7a77
suraifokkusu <79616065+suraifokkusu@users.noreply.github.com>
2022-02-03 17:43:58 +1000

    get all products && get by id && post add product

---
+++ TA_01/GetAllProduct/src/main/java/suraifokkusu/GetAllProductApplication.java
@@ -0,0 +1,13 @@
+package suraifokkusu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetAllProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetAllProductApplication.class, args);
+    }
+
+}
