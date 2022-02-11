9f276bf2c3f1381e09e2dfc7e0a6345ce42dcaba
EvoMGenius <apatrios@mail.ru>
2022-02-11 19:48:14 +1000

    Add auto initialize admin account

--- dbInitializer/src/main/java/com/evo/apatrios/dbinitializer/DbInitializerApplication.java
+++ dbInitializer/src/main/java/com/evo/apatrios/dbinitializer/DbInitializerApplication.java
@@ -1,7 +1,11 @@
 package com.evo.apatrios.dbinitializer;
 
+import com.evo.apatrios.dbinitializer.models.Account;
+import com.evo.apatrios.dbinitializer.models.AccountRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DbInitializerApplication {
@@ -10,4 +14,15 @@ public class DbInitializerApplication {
         SpringApplication.run(DbInitializerApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner run(AccountRepository repository){
+        return args -> {
+            Account admin = new Account();
+            admin.setEmail("admin@mail.ru");
+            admin.setMoney(0);
+            admin.setRole("Admin");
+            admin.setPassword("21232f297a57a5a743894a0e4a801fc3");
+            repository.save(admin);
+        };
+    }
 }
