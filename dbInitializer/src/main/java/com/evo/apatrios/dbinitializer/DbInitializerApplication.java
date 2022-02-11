263800543cb1de46e40bca23dd465271f619c094
EvoMGenius <apatrios@mail.ru>
2022-02-11 18:40:00 +1000

    Did initializer of db with: orders, accounts, products in order. This initializer will autodrop and rebuild all tables in db

---
+++ dbInitializer/src/main/java/com/evo/apatrios/dbinitializer/DbInitializerApplication.java
@@ -0,0 +1,13 @@
+package com.evo.apatrios.dbinitializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbInitializerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbInitializerApplication.class, args);
+    }
+
+}
